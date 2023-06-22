1afb16df20045c8a8da5ceb06db61e799a4d6d1c
AmriTSalahudin <amri.salahudin@gmail.com>
2023-06-22 07:01:19 +0700

    belajar golang

---
+++ hist/funcparam.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// func param
+func hallo(kataPertama string, kataKedua string) {
+	fmt.Println("hallo jon", kataPertama, kataKedua)
+}
+
+func main() {
+
+	hallo("kita", "makan")
+}
